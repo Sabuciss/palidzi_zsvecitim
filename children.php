2e0ec3c31960e559db80028534130fedf92cf13f
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2024-12-17 14:11:19 +0200

    Update children.php

--- children.php
+++ children.php
@@ -1,27 +1,23 @@
 <?php
-
-
-echo "<link rel='stylesheet ' href='style.css' >";
+echo "<link rel='stylesheet' href='style.css'>";
 require "Database.php";
 
 $config = require("config.php");
 
-echo "Hi there  <br><br>";
-
 $db = new Database($config["database"]);
 
 $kids = $db->query("SELECT * FROM children")->fetchAll();
+$letters = $db->query("SELECT * FROM letters")->fetchAll();
+
 foreach ($kids as $kid) {
-    echo "<div class='card'>";
-    echo "<h3>" . htmlspecialchars($kid["firstname"]) . " " . htmlspecialchars($kid["middlename"]) . " " . htmlspecialchars($kid["surname"]) . "</h3>";
-    echo "<p>Age: " . htmlspecialchars($kid["age"]) . "</p>";
-    echo "</div>";
+    foreach ($letters as $letter) {
+        if ($letter["sender_id"] == $kid["id"]) {
+            echo "<div class='card'>";
+            echo "<h3>" . htmlspecialchars($kid["firstname"]) . " " . htmlspecialchars($kid["middlename"]) . " " . htmlspecialchars($kid["surname"]) . "</h3>";
+            echo "<p>Age: " . htmlspecialchars($kid["age"]) . "</p>";
+            echo "<p>Vestule: " . htmlspecialchars($letter["letter_text"]) . "</p>";  
+            echo "</div>"; 
+        }
+    }
 }
-
-echo "</div>";
-
-
-   
-
-
 ?>
